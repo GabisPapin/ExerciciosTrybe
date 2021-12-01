099e8e7755206f6c89aa2a068b7d706bcf81c717
Gabriela Papin <gabrielapapin20@gmail.com>
2021-12-01 13:39:37 -0300

    revisando exercicio bonus 4

--- fundamentos/bloco4-introducao-a-JS-e-logica-de-programacao/dia3-JavaScript-logica-de-programacao-e-algoritmo/algoritmo.js
+++ fundamentos/bloco4-introducao-a-JS-e-logica-de-programacao/dia3-JavaScript-logica-de-programacao-e-algoritmo/algoritmo.js
@@ -21,7 +21,7 @@ for(cc = 0; cc <= base; cc += 1){
 }*/
 
 //Exercicio bonus 3 = inverta o lado do triângulo retângulo com 5 asteriscos de base.
-let base = 5;
+/*let base = 5;
 let simbolo = '*';
 let linhas = '';
 let posicao = base;
@@ -37,7 +37,30 @@ for(let linhaCont = 0; linhaCont <= base; linhaCont += 1){
     console.log(linhas);
     linhas = '';
     posicao = posicao - 1;
+}*/
+
+//Exercicio bonus 4: faça uma pirâmide com n asteriscos de base.
+let base = 5;
+let simbolo = '*';
+let linhas = '';
+let meioPir = 3; //(base + 1)/2
+let ladoEsq = meioPir;
+let ladoDir = meioPir;
+
+for(let linhaCont = 0; linhaCont <= meioPir; linhaCont += 1){
+    for(let colunaCont = 0; colunaCont <= base; colunaCont += 1){
+        if(colunaCont > ladoEsq && colunaCont < ladoDir){
+            linhas = linhas + simbolo;
+        }else{
+            linhas = linhas + " ";
+        }
+    }
+    console.log(linhas);
+    linhas = '';
+    ladoDir += 1;
+    ladoEsq -= 1;
 }
+
 
 /*let n = 7;
 
