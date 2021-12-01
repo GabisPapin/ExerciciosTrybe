efcfd2d52538917eb935997eee0858759b954516
Gabriela Papin <gabrielapapin20@gmail.com>
2021-11-30 23:24:19 -0300

    resolvendo exercicio bonus

---
+++ fundamentos/bloco4-introducao-a-JS-e-logica-de-programacao/dia3-JavaScript-logica-de-programacao-e-algoritmo/algoritmo.js
@@ -0,0 +1,44 @@
+//let n = 5;
+
+/*for(cc = 0; cc < n; cc += 1){
+    console.log("*****");
+}*/
+
+/*for(cc = 0; cc <= n; cc += 1){
+    if(cc === 0){
+        console.log("     " + "*");
+    } if(cc === 2){
+        console.log("    " + "**");
+    } if(cc === 3){
+        console.log("   " + "***");
+    } if(cc === 4) {
+        console.log("  " + "****");
+    } if(cc === 5) {
+        console.log(" " + "*****");
+    }
+}*/
+
+/*for(cc = 0; cc <= n; cc += 1){
+    if(cc === 0){
+        console.log("  " + "*" + "  ");
+    }if(cc === 3){
+        console.log(" " + "***" + " ");
+    }else if(cc === 5){
+        console.log("*****");
+    }
+}*/
+
+/*let n = 7;
+
+for(cc = 0; cc <= 7; cc += 1){
+    if(cc === 1){
+        console.log("  " + "*" + "  ");
+    }
+    if(cc === 1){
+        console.log(" " + "*" + " " + "*" + " ");
+    }if(cc === 3){
+        console.log(" " + "*" + "  " + "*" + " ");
+    }else if(cc === 5){
+        console.log("*******");
+    }
+}*/
