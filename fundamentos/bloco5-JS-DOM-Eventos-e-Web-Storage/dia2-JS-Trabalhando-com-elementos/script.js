f56d1180341f89d1266b2ec88806effe08707761
Gabriela Papin <gabrielapapin20@gmail.com>
2021-12-07 12:59:51 -0300

    anotaçoes no arquivo script.js

---
+++ fundamentos/bloco5-JS-DOM-Eventos-e-Web-Storage/dia2-JS-Trabalhando-com-elementos/script.js
@@ -0,0 +1,7 @@
+// arquivo script.js
+
+console.log(document.getElementById('start').nextSibling) // nó
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+//O arquivo html não está chamando o script pois estou testando outro código utilizando
+//a tag script.
